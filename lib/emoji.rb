aa161194bf5cdb88cb1fdaf9cf26f573d8708af2
Mislav Marohnić <mislav.marohnic@gmail.com>
2014-03-13 01:39:40 +0100

    Get emoji list & aliases from data file instead of symlinks
    Previously, emoji name & unicode aliases were determined by following
    symlinks among `images/emoji/*.png`. This led to nontrivial code for
    resolving these aliases, made it tricky for contributors to add new
    aliases and inspect existing ones, and didn't leave room for adding
    metadata to emojis such as tags or descriptions from the Unicode spec.
    
    Moreover, the aliases as symlinks led to duplication of image assets in
    users' applications, with `hocho.png` and `knife.png` representing the
    same emoji but being two separate images. Users were also unsure what to
    do with `unicode/{HEX-NAME}.png` files, which would end up among their
    images after running the `:emoji` task.
    
    This change removes the symlinks support and creates the list of emojis
    and their aliases in `emoji.txt`. A single emoji is now represented with
    an Emoji::Character instance, which has the `image_filename` method to
    determine the path to the corresponding image instead of having to
    construct it manually.

--- lib/emoji.rb
+++ lib/emoji.rb
@@ -1,65 +1,114 @@
+require 'emoji/character'
+
 module Emoji
   extend self
+
+  def data_file
+    File.expand_path('../../db/emoji.txt', __FILE__)
+  end
 
   def images_path
     File.expand_path("../../images", __FILE__)
   end
 
+  def all
+    @all ||= parse_data_file
+  end
+
+  def find_by_alias(name)
+    names_index.fetch(name) { yield }
+  end
+
+  def find_by_unicode(unicode)
+    unicodes_index.fetch(unicode) { yield }
+  end
+
   def names
-    @names ||= mapping.keys.sort
+    @names ||= names_index.keys.sort
   end
 
   def unicodes
-    @unicodes ||= inverted_mapping.keys
+    @unicodes ||= unicodes_index.keys
   end
 
   def custom
-    @custom ||= mapping.select { |name, unicode| unicode.nil? }.keys.sort
+    @custom ||= all.map { |emoji|
+      emoji.aliases if emoji.custom?
+    }.compact.flatten.sort
   end
 
   def unicode_for(name)
-    Array(mapping[name]).last
+    emoji = find_by_alias(name) { return nil }
+    emoji.raw
   end
 
   def name_for(unicode)
-    inverted_mapping[unicode]
+    emoji = find_by_unicode(unicode) { return nil }
+    emoji.name
+  end
+
+  def names_for(unicode)
+    emoji = find_by_unicode(unicode) { return nil }
+    emoji.aliases
   end
 
   private
-    def mapping
-      @mapping ||= {}.tap do |mapping|
-        emoji_path = "#{images_path}/emoji"
+    def create_index
+      index = Hash.new { |hash, key| hash[key] = [] }
+      yield index
+      index
+    end
 
-        Dir["#{emoji_path}/*.png"].each do |filename|
-          name = File.basename(filename, ".png")
+    def parse_data_file
+      emojis = []
 
-          if File.symlink?(filename)
-            unicode_filename = "#{emoji_path}/#{File.readlink(filename)}"
-            mapping[name] = []
-
-            loop do
-              codepoints = unicode_filename.match(/unicode\/([\da-f\-]+)\.png/)[1]
-              mapping[name] << codepoints.split("-").map(&:hex).pack("U*")
-
-              if File.symlink?(unicode_filename)
-                unicode_filename = "#{emoji_path}/unicode/#{File.readlink(unicode_filename)}"
-              else
-                break
-              end
-            end
+      File.open(data_file, 'r:UTF-8') do |file|
+        char = nil
+        file.each_line do |line|
+          case line
+          when /^(\S+)\s+\((.+?)\)/
+            emoji, hex = $1, $2
+            name, emoji = emoji, nil if 'custom' == hex
+            char = Emoji::Character.new(emoji)
+            char.add_alias(name) if name
+            emojis << char
+          when /^\s*=/
+            aliases = $'.strip.split(/\s*,\s*/)
+            aliases.each { |name| char.add_alias(name) }
+          when /^\s*\+/
+            unicodes = $'.strip.split(/\s*,\s*/)
+            unicodes.each { |codes|
+              raw = codes.split('-').map(&:hex).pack('U*')
+              char.add_unicode_alias(raw)
+            }
           else
-            mapping[name] = nil
+            raise line.inspect
+          end
+        end
+      end
+
+      emojis
+    end
+
+    def names_index
+      @names_index ||= create_index do |mapping|
+        all.each do |emoji|
+          unicodes = emoji.unicode_aliases.dup
+          unicodes << emoji.raw unless emoji.custom?
+          emoji.aliases.each do |name|
+            mapping[name] = emoji
           end
         end
       end
     end
 
-    def inverted_mapping
-      @inverted_mapping ||= {}.tap do |inverted_mapping|
-        mapping.each do |name, unicodes|
-          next if unicodes.nil?
+    def unicodes_index
+      @unicodes_index ||= create_index do |mapping|
+        all.each do |emoji|
+          unicodes = emoji.unicode_aliases.dup
+          unicodes << emoji.raw unless emoji.custom?
           unicodes.each do |unicode|
-            inverted_mapping[unicode] = name
+            mapping[unicode] = emoji
           end
         end
       end
